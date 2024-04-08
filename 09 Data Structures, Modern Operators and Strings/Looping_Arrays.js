d4a14fbada48a407dd13223f3c581d121b442482
Aliza Razi <aliza302001@gmail.com>
2024-04-09 04:37:56 +0530

    first commit

--- 09 Data Structures, Modern Operators and Strings/Looping_Arrays.js
+++ 09 Data Structures, Modern Operators and Strings/Looping_Arrays.js
@@ -88,3 +88,59 @@ staff.add('customer')
 console.log(staff);
 
 //------ DATA STRUCTURE:  "Map"----------------
+
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+    .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+
+// rest.clear();
+
+const question = new Map([
+    ['question', 'What is the best programming language in the world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'Correct 🎉'],
+    [false, 'Try again!'],
+]);
+
+console.log(question);
+
+
+const hourmap = new Map(Object.entries(restaurant.openingHours));
+console.log(hourmap);
+
+
+for (const [key, value] of question) {
+    if (typeof key === 'number') {
+        console.log(`Answer ${key}:${value}`);
+    }
+}
+
+//const ans = Number(prompt('Your answer'));
+// console.log(ans);
+const ans = 3;
+console.log(question.get(question.get('correct') === ans));
+
+
+//convet map to array
+console.log([...question]);
