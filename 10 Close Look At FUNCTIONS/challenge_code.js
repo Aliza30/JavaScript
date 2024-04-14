f4812b25d6cad0664a705faf843320a145cc8605
Aliza Razi <aliza302001@gmail.com>
2024-04-14 20:25:43 +0530

    Initial commit

--- 10 Close Look At FUNCTIONS/challenge_code.js
+++ 10 Close Look At FUNCTIONS/challenge_code.js
@@ -15,7 +15,7 @@ Here are your tasks:
         2: Rust
         3: C++
         (Write option number)
-  
+
   1.2. Based on the input number,update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
 2. Call this method whenever the user clicks the "Answer poll" button.
 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
@@ -71,3 +71,26 @@ document
 
 poll.displayResult.call({ answers: [5, 3, 1] }, 'string');
 // we use call to call displayresult to manally set new object to new array.
+
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    })
+})();
+
+/**
+ *   document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    })
+
+
+    header====> is working as a clousure.....
+
+
+    this is call back......
+ */
+
